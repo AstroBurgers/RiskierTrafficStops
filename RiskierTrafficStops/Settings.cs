134c48fe8cb9443ea87147cf692f0c6a87e78b92
Astro <astroburgers1@gmail.com>
2023-05-27 01:00:06 -0500

    Updated INI

--- RiskierTrafficStops/Settings.cs
+++ RiskierTrafficStops/Settings.cs
@@ -14,6 +14,14 @@ namespace RiskierTrafficStops
         internal static Keys GetBackIn = Keys.Y;
         internal static InitializationFile inifile; // Defining a new INI File
 
+        // If events are enabled
+        internal static bool GOAS = true;
+        internal static bool Ram = true;
+        internal static bool Flee = true;
+        internal static bool Rev = true;
+        internal static bool Yell = true;
+        internal static bool YIC = true;
+
         internal static void INIFile()
         {
             inifile = new InitializationFile(@"Plugins/Lspdfr/RiskierTrafficStops.ini");
@@ -21,6 +29,14 @@ namespace RiskierTrafficStops
 
             Chance = inifile.ReadInt32("Settings", "Chance", 15);
             GetBackIn = inifile.ReadEnum("Settings", "Keybind", GetBackIn);
+
+            // Bools for events
+            GOAS = inifile.ReadBoolean("Settings", "Get Out And Shoot Outcome enabled", GOAS);
+            Ram = inifile.ReadBoolean("Settings", "Ramming Oucome enabled", Ram);
+            Flee = inifile.ReadBoolean("Settings", "Flee Outcome enabled", Flee);
+            Rev = inifile.ReadBoolean("Settings", "Revving Outcome enabled", Rev);
+            Yell = inifile.ReadBoolean("Settings", "Yelling Outcome enabled", Yell);
+            YIC = inifile.ReadBoolean("Settings", "Yelling Car in Outcome enabled", YIC);
         }
     }
 }
