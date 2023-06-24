40411958bd1298c68fb62bce330077e67fbaa2a7
Astro <astroburgers1@gmail.com>
2023-06-24 12:48:54 -0500

    Updated SAF

--- RiskierTrafficStops/Outcomes/ShootAndFlee.cs
+++ RiskierTrafficStops/Outcomes/ShootAndFlee.cs
@@ -19,18 +19,24 @@ namespace RiskierTrafficStops.Outcomes
         internal static LHandle PursuitLHandle;
         internal static void SAFOutcome(LHandle handle)
         {
-            // Asigning the Suspect and the Suspect vehicle
             Debug("Setting up Suspect");
+
             Suspect = Functions.GetPulloverSuspect(handle);
             Debug("Setting up suspectVehicle");
             suspectVehicle = Suspect.CurrentVehicle;
             Suspect.BlockPermanentEvents = true;
             Suspect.IsPersistent = true;
             suspectVehicle.IsPersistent = true;
-            // Adding all suspects to the List
+
             Debug("Adding all suspect in the vehicle to a list");
+
             List<Ped> PedsInVehicle = GetAllVehicleOccupants(suspectVehicle);
             Debug($"Peds In Vehicle: {PedsInVehicle.Count}");
+
+            Debug("Setting up SuspectRelateGroup");
+
+            SuspectRelateGroup.SetRelationshipWith(MainPlayer.RelationshipGroup, Relationship.Hate);
+            SuspectRelateGroup.SetRelationshipWith(RelationshipGroup.Cop, Relationship.Hate);
         }
 
 
