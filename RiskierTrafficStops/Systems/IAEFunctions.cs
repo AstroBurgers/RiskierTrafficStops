880b57e18dfba3c612e4ab35be51fc345349fba6
Echooo <need2speed29@gmail.com>
2023-10-25 21:27:11 -0400

    Updated IAE APIs

--- RiskierTrafficStops/Systems/IAEFunctions.cs
+++ RiskierTrafficStops/Systems/IAEFunctions.cs
@@ -8,15 +8,15 @@ namespace RiskierTrafficStops.Systems
     internal class IAEFunctions
     {
         /// <summary>
-        /// Checks if an entity is being used by IAE
+        /// Checks if an entity is being used by a normal IAE event
         /// </summary>
         /// <param name="entity"></param>
         /// <returns></returns>
-        internal static bool IsPedUsedByAnyAmbientEvent(Entity entity)
+        internal static bool IsPedUsedByAmbientEvent(Entity entity)
         {
             try
             {
-                return ImmersiveAmbientEvents.API.EventAPI.IsEntityUsedByAnyEvent(entity) || ImmersiveAmbientEvents.API.BoloEventAPI.IsEntityUsedByAnyBOLOEvent(entity);
+                return ImmersiveAmbientEvents.API.EventAPI.IsEntityUsedByAnyEvent(entity);
             }
             catch (FileNotFoundException)
             {
@@ -30,8 +30,55 @@ namespace RiskierTrafficStops.Systems
             }
         }
 
+
         /// <summary>
-        /// Checks if a pullover is a part of an IAE event, returns false if so
+        /// Checks if an entity is being used by an IAE BOLO event
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <returns></returns>
+        internal static bool IsPedUsedByBOLOEvent(Entity entity)
+        {
+            try
+            {
+                return ImmersiveAmbientEvents.API.BoloEventAPI.IsEntityUsedByAnyBOLOEvent(entity);
+            }
+            catch (FileNotFoundException)
+            {
+                Logger.Debug("Immersive Ambient Events cannot be found, user might not have it installed");
+                return false;
+            }
+            catch (Exception ex)
+            {
+                Logger.Error(ex, "IAEFunctions.cs, IsPedUsedByBOLOEvent()");
+                return false;
+            }
+        }
+
+        internal static bool IAEEventCheck()
+        {
+            try
+            {
+                if (ImmersiveAmbientEvents.API.EventAPI.CheckIfEventIsRunning(ImmersiveAmbientEvents.API.EventAPI.Events.StreetRacing))
+                {
+                    Logger.Debug("Pullover is a part of an IAE street racing event, aborting RTS events...");
+                    return false;
+                }
+                return true;
+            }
+            catch (FileNotFoundException)
+            {
+                Logger.Debug("Immersive Ambient Events cannot be found, user might not have it installed");
+                return false;
+            }
+            catch (Exception ex)
+            {
+                Logger.Error(ex, "IAEFunctions.cs, IAEEventCheck()");
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Handles all compatibility checks for IAE, returns true if it passes the checks
         /// </summary>
         /// <param name="handle"></param>
         /// <returns></returns>
@@ -40,9 +87,13 @@ namespace RiskierTrafficStops.Systems
             try
             {
                 Ped ped = Functions.GetPulloverSuspect(handle);
-                if (IsPedUsedByAnyAmbientEvent(ped))
+                if (IsPedUsedByAmbientEvent(ped))
                 {
-                    Logger.Debug("Pullover is a part of an IAE event, aborting RTS events...");
+                    return IAEEventCheck();
+                }
+                else if (IsPedUsedByBOLOEvent(ped))
+                {
+                    Logger.Debug("Pullover is a part of an IAE BOLO event, aborting RTS events...");
                     return false;
                 }
                 return true;
