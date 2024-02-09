2e564cce999268a918b6d4fed6eb733a21341146
AstroBurgers <astroburgers1@gmail.com>
2024-02-08 19:49:06 -0600

    Added Localization.cs and Localization.ini

---
+++ RiskierTrafficStops/Engine/InternalSystems/Localization.cs
@@ -0,0 +1,13 @@
+﻿namespace RiskierTrafficStops.Engine.InternalSystems;
+
+internal static class Localization
+{
+    internal static InitializationFile LocalizationIniFile; // Defining a new INI File
+
+    
+    
+    internal static void SetupLocalization()
+    {
+        
+    }
+}
