b571014fc5e7867d29e09508afd5af1ac7e54d71
AstroBurgers <astroburgers1@gmail.com>
2024-02-08 19:45:29 -0600

    Moved spitting text array to Arrays.cs

--- RiskierTrafficStops/Mod/Outcomes/Spitting.cs
+++ RiskierTrafficStops/Mod/Outcomes/Spitting.cs
@@ -23,34 +23,6 @@ internal class Spitting : Outcome
         }
     }
 
-    private static readonly string[] SpittingText =
-    {
-        "~y~Suspect: ~s~*spits at you* Fuck you pig",
-        "~y~Suspect: ~s~*spits at you* Bitch",
-        "~y~Suspect: ~s~*spits at you* Come on lets fight!",
-        "~y~Suspect: ~s~*spits at you* Motherfucker",
-        "~y~Suspect: ~s~*spits at you* Shit I didn't mean to hit you officer",
-        "~y~Suspect: ~s~*spits at you* Damnit I didn't see you there",
-        "~y~Suspect: ~s~*spits at you* ACAB!",
-        "~y~Suspect: ~s~*spits at you and misses* You little bitch",
-        "~y~Suspect: ~s~*spits at you and misses* Agh what did I do now",
-        "~y~Suspect: ~s~*spits at you and misses* Ope sorry!",
-        "~y~Suspect: ~s~*spits at you and hits badge* Haha little bitch",
-        "~y~Suspect: ~s~*spits at you and hits badge* I should shoot you for pulling me over",
-        "~y~Suspect: ~s~*spits at you and hits badge* I AM SO SORRY OFFICER",
-        "~y~Suspect: ~s~*spits at you and hits badge* Oh fuck off",
-        "~y~Suspect: ~s~*spits at you and hits shoe* ACAB Bitch!",
-        "~y~Suspect: ~s~*spits at you and hits shoe* Fuckin pig",
-        "~y~Suspect: ~s~*spits at you and hits shoe* Fucking pig",
-        "~y~Suspect: ~s~*spits at you and hits shoe* Your a bitch you know that?",
-        "~y~Suspect: ~s~*spits at you and hits shoe* Screw you pig",
-        "~y~Suspect: ~s~*spits at you and hits shoe* What are you gonna do now, huh?",
-        "~y~Suspect: ~s~*spits at you and hits shoe* Whatcha gonna do you little bitch?",
-        "~y~Suspect: ~s~*spits at you and hits shoe* Where's your little squad of bitches?",
-        "~y~Suspect: ~s~*spits at you and hits gun* Oh look the bitch patrol!",
-        "~y~Suspect: ~s~*spits at you and hits taser* Oh look the bitch patrol!",
-    };
-
     internal override void StartOutcome()
     {
         InvokeEvent(RTSEventType.Start);
