5615d53e40a6ae52e3b7b8b00594f15f6526eac7
AstroBurgers <astroburgers1@gmail.com>
2024-02-10 21:30:31 -0600

    Added HostageTaking.cs

---
+++ RiskierTrafficStops/Mod/Outcomes/HostageTaking.cs
@@ -0,0 +1,37 @@
+﻿namespace RiskierTrafficStops.Mod.Outcomes;
+
+internal class HostageTaking : Outcome
+{
+    public HostageTaking(LHandle handle) : base(handle)
+    {
+        try
+        {
+            if (MeetsRequirements(TrafficStopLHandle))
+            {
+                GameFiberHandling.OutcomeGameFibers.Add(GameFiber.StartNew(StartOutcome));
+            }
+        }
+        catch (Exception e)
+        {
+            if (e is ThreadAbortException) return;
+            Error(e, nameof(StartOutcome));
+            CleanupOutcome(true);
+        }
+    }
+
+    internal override void StartOutcome()
+    {
+        InvokeEvent(RTSEventType.Start);
+            
+        Normal("Getting all vehicle occupants");
+        var pedsInVehicle = SuspectVehicle.Occupants.ToList();
+
+        foreach (var ped in pedsInVehicle)
+        {
+            if (ped.IsAvailable() && PedsToIgnore.Contains(ped))
+            {
+                pedsInVehicle.Remove(ped);
+            }
+        }
+    }
+}
