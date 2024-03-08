51529cd5d05e58e3207ac11b5f59bf2fbc733a21
AstroBurgers <astroburgers1@gmail.com>
2024-03-07 19:19:46 -0600

    Added HandleIgnoredPedsList

--- RiskierTrafficStops/Engine/InternalSystems/Processes.cs
+++ RiskierTrafficStops/Engine/InternalSystems/Processes.cs
@@ -1,6 +1,20 @@
-﻿namespace RiskierTrafficStops.Engine.InternalSystems;
+﻿using static RiskierTrafficStops.Mod.Outcome;
+
+namespace RiskierTrafficStops.Engine.InternalSystems;
 
 internal class Processes
 {
-    
+    internal static void HandleIgnoredPedsList()
+    {
+        while (Main.OnDuty)
+        {
+            GameFiber.Yield();
+            GameFiber.Wait(600000);
+            if (PedsToIgnore is null) continue;
+            foreach (var ped in PedsToIgnore.Where(i => !i.IsAvailable()))
+            {
+                PedsToIgnore.Remove(ped);
+            }
+        }
+    }
 }
