047f3a58408a1dfb5bbba0ded4386a7e18f7fe1d
AstroBurgers <astroburgers1@gmail.com>
2024-03-10 16:18:49 -0500

    Optimized GivePistol

--- RiskierTrafficStops/Engine/Helpers/Extensions/PedExtensions.cs
+++ RiskierTrafficStops/Engine/Helpers/Extensions/PedExtensions.cs
@@ -4,18 +4,10 @@ internal static class PedExtensions
 {
     internal static void GivePistol(this Ped ped)
     {
-        if (ped.IsAvailable() && !ped.Inventory.HasLoadedWeapon)
-        {
-            var weapon = PistolList[Rndm.Next(PistolList.Length)];
-            ped.Inventory.GiveNewWeapon(weapon, 100, true);
-            Normal($"Giving {ped.Model.Name} {weapon}");
-        }
-        else if (ped.IsAvailable() && ped.Inventory.HasLoadedWeapon)
-        {
-            var pedWeapons = ped.Inventory.Weapons;
-            var weapon = pedWeapons[Rndm.Next(pedWeapons.Count)];
-            ped.Inventory.EquippedWeapon = weapon.ToString();
-        }
+        if (!ped.IsAvailable()) return;
+        var pedWeapons = ped.Inventory.Weapons;
+        var weapon = ped.Inventory.HasLoadedWeapon ? pedWeapons[Rndm.Next(pedWeapons.Count)] : PistolList[Rndm.Next(PistolList.Length)];
+        Normal($"Giving {ped.Model.Name} {weapon}");
     }
     
     /// <summary>
