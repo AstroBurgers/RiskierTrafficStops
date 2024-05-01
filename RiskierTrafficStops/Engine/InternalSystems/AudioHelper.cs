a2560683f343bd18b9035a529dc2ddfd2b414681
AstroBurgers <astroburgers1@gmail.com>
2024-05-01 13:48:55 -0500

    Create AudioHelper.cs

---
+++ RiskierTrafficStops/Engine/InternalSystems/AudioHelper.cs
@@ -0,0 +1,19 @@
+﻿using IrrKlang;
+
+namespace RiskierTrafficStops.Engine.InternalSystems;
+
+internal class SoundPlayer
+{
+    internal string FileName;
+    private ISoundEngine soundEngine = new();
+        
+    internal SoundPlayer(string FileName)
+    {
+        this.FileName = FileName;
+    }
+
+    internal void PlaySound2D()
+    {
+        soundEngine.Play2D(FileName);
+    }
+}
