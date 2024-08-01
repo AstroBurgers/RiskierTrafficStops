4d5f06b11cee8cc0f1cf7090a4a5509b62ce9c1f
AstroBurgers <astroburgers1@gmail.com>
2024-08-01 16:45:22 -0500

    Fixed copyright

--- RiskierTrafficStops/Properties/AssemblyInfo.cs
+++ RiskierTrafficStops/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("RiskierTrafficStops")]
-[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyCopyright("Copyright AstroBurgers©  2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
